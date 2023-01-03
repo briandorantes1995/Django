1bcc4c36e6194a14e9d73d65ebb3e9bcd8121917
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-01-03 02:59:05 -0600

    Dynamic month list and reverse and dynamical url

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("",views.month_list),
     path("<int:month>",views.monthly_challenge_bynumber),
-    path("<str:month>",views.monthly_challenge)
+    path("<str:month>",views.monthly_challenge,name="month_challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 monthly_challenges = {
@@ -21,15 +22,27 @@ monthly_challenges = {
 def monthly_challenge_bynumber(request,month):
     months = list(monthly_challenges.keys())
     if(month) > len(months):
-        return HttpResponseRedirect("/challenges/"+"notfound")
+        return HttpResponseRedirect("/challenge/"+"notfound")
     forward_month = months[month-1]
-    return HttpResponseRedirect("/challenges/"+forward_month)
+    redirect_month = reverse("month_challenge",args=[forward_month]) #/challenge
+    return HttpResponseRedirect(redirect_month)
 
 
 def monthly_challenge(request,month):
     try:
         challenge_text = monthly_challenges[month.lower()]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("Month not available")
 
+
+def month_list(request):
+    list_items =""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        redirect_month = reverse("month_challenge",args=[month]) #/challenge
+        list_items += f"<li><a href=\"{redirect_month.capitalize()}\">{month}</a></li>"
+    
+    response_data =f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
